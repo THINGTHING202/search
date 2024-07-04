dd1de287e00e4574c7552f56e51211dacb9afda3
THINGTHING202 <72882140+THINGTHING202@users.noreply.github.com>
2024-07-04 00:16:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.body.style.background = 'url("https://i.ibb.co/Qf0HzSX/914611-anime-anime-girls-digital-art-artwork-2-D-portrait.png")';
+document.body.style.backgroundSize = '156% 962%'; // Adjusts the background image size to cover the entire background
+document.body.style.backgroundPosition = '13% 30%'; // Shifts the background image to center it
